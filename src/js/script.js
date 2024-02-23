9ed3fabe71e73682e45ad0b00d61f0ffbf1a3fdd
Hian A. Damaceno <132616428+55Hian@users.noreply.github.com>
2024-02-23 15:41:29 +0000

    .

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+constroiCabecalho;
+itensCabecalho = ["ServiceNow", "item 2", "item 3", "item 4"]
+
+function constroiCabecalho(){
+    botaoCabecalho = 
+    `
+    
+    <div class="gridItem">
+    <div ontouchstart="">
+        <div class="button">
+          <a href="./src/html/serviceNow.html"><img width="100px" src="./src/img/62b459b44d5b68158481cb1a.png" alt=""></a>
+        </div>
+      </div>
+    </div>
+    
+    `
+
+
+    
+}
